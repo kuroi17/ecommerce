90893c59cfdc283749ea1afe40681068c41864cc
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-08-31 07:35:49 +0000

    done the onject and array part for menu

--- main.js
+++ main.js
@@ -1,9 +1,84 @@
 const menuData = {
     mainDishes: [
-        name: 'Jolli-Burger',
+       
+       {
+        name: '1PC ChickenJoy',
         price: '66.00',
-        image: 'images/button-addtocart.png ',
+        image: 'images/jolibee-main-meals/jolli-friedchicken.png ',
         id: 'chickenjoy'
 
+       },
+       {
+         name: '1PC Jolli-spaghetti',
+        price: '66.00',
+        image: 'images/jolibee-main-meals/jolli-spaghetti.png',
+        id: 'spaghetti'
+       }
+       
+       {
+        name: '1PC Burgersteak',
+        price: '66.00',
+        image: 'images/jolibee-main-meals/burgersteak.png',
+        id: 'burgersteak'
+       }
+
+       {
+        name: 'Jolli-Burger',
+        price: '66.00',
+        image: 'images/jolibee-main-meals/burger.png',
+        id: 'burger'
+       }
+
+       {
+        name: '1PC Jolli-Fries',
+        price: '66.00',
+        image: 'images/jolibee-main-meals/fries.png',
+        id: 'fries'
+       }
+    ],
+
+    drinks: [
+        {
+        name: 'Jolibee Coke',
+        price: '66.00',
+        image: 'images/jolibee-drinks/cokes.jpg',
+        id: 'coke'
+        },
+        {
+        name: 'Pineapple Juice',
+        price: '66.00',
+        image: 'images/jolibee-drinks/Pineapple-Juice-Regular.webp',
+        id: 'pineapple'
+        },
+        {
+        name: 'Water',
+        price: '66.00',
+        image: 'images/jolibee-drinks/Pineapple-Juice-Regular.webp',
+        id: 'water'
+        }
+
+
+    ],
+
+    desserts: [
+    {
+        name: 'Mango Pie',
+        price: '66.00',
+        image: 'images/jolibee-desserts/mango pie.png',
+        id: 'mangopie'
+    }
+    {
+        name: 'Joli-sundae',
+        price: '66.00',
+        image: 'images/jolibee-desserts/sundae.png',
+        id: 'pineapple'
+    }
+    {
+        name: 'Coke Float',
+        price: '66.00',
+        image: 'images/jolibee-drinks/cokefloat.webp',
+        id: 'cokefloat'
+    }
+
     ]
-}
+};
