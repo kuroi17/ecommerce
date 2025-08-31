27d6dc8dbad46975294afa38580dd3dea2e1e583
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-08-31 07:14:28 +0000

    trying to do the js part

--- main.js
+++ main.js
@@ -1,30 +1,9 @@
-const images = document.querySelectorAll(".banner-image");
+const menuData = {
+    mainDishes: [
+        name: 'Jolli-Burger',
+        price: '66.00',
+        image: 'images/button-addtocart.png ',
+        id: 'chickenjoy'
 
-const size = images.length;
-let currentindex = 0;
-
-function showImage(currentindex) {
-  for (let i = 0; i < size; i++) {
-    images[i].classList.remove("active");
-  }
-
-  images[currentindex].classList.add("active");
+    ]
 }
-showImage(currentindex);
-
-document.querySelector(".next").addEventListener("click", function () {
-  currentindex = (currentindex + 1) % size;
-  showImage(currentindex);
-});
-
-document.querySelector(".prev").addEventListener("click", () => {
-  currentindex = (currentindex - 1 + size) % size;
-
-  showImage(currentindex);
-});
-
-setInterval(() => {
-  currentindex = (currentindex + 1) % size;
-
-  showImage(currentindex);
-}, 4000);
